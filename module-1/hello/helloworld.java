5522674111b0f539c1998f6a03eef28879ffe6a5
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-06 15:40:11 +0500

    hello

--- module-1/hello/helloworld.java
+++ module-1/hello/helloworld.java
@@ -1,7 +1,9 @@
-package hello;
-
+/**
+ * HelloWorld
+ */
 public class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("helloWorld");
     }
 }
